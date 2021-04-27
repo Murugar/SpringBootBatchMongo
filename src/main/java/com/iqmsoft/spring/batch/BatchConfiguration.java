c7eb594a111ab38b50b9fa7e705fcd5452394f3e
Muruga <davanon2014@gmail.com>
2021-04-27 11:20:45 +0530

    Up

--- src/main/java/com/iqmsoft/spring/batch/BatchConfiguration.java
+++ src/main/java/com/iqmsoft/spring/batch/BatchConfiguration.java
@@ -2,15 +2,13 @@ package com.iqmsoft.spring.batch;
 
 import com.iqmsoft.spring.batch.model.Person;
 import com.iqmsoft.spring.batch.processor.PersonItemProcessor;
-import com.iqmsoft.spring.batch.repos.PersonRepository;
 import com.iqmsoft.spring.batch.tokenizer.PersonFixedLengthTokenizer;
-import com.mongodb.DBAddress;
-import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClients;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -23,21 +21,19 @@ import org.springframework.batch.item.data.MongoItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
-	
-	
 
-	
 	private static final Logger logger = LoggerFactory.getLogger(BatchConfiguration.class);
 
 	@Bean
@@ -106,20 +102,24 @@ public class BatchConfiguration {
 	@Bean
 	public Step step1(StepBuilderFactory stepBuilderFactory, ItemReader<Person> reader, ItemWriter<Person> writer,
 			ItemProcessor<Person, Person> processor) {
-		return stepBuilderFactory.get("step1").<Person, Person>chunk(10)
-				.reader(reader).processor(processor)
+		return stepBuilderFactory.get("step1").<Person, Person>chunk(10).reader(reader).processor(processor)
 				.writer(writer).build();
 	}
 
 	@Bean
-	public MongoDbFactory mongoDbFactory() throws Exception {
-		return new SimpleMongoDbFactory(new MongoClient(), "batchdb");
+	public SimpleMongoClientDatabaseFactory mongoDbFactory() throws Exception {
+		return new SimpleMongoClientDatabaseFactory(MongoClients.create(), "batchdb");
 	}
 
 	@Bean
 	public MongoTemplate mongoTemplate() throws Exception {
 		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
 		return mongoTemplate;
+	}
+
+	@Bean
+	public MongoOperations mongoOperations() throws Exception {
+		return new MongoTemplate(mongoDbFactory());
 	}
 
 }
